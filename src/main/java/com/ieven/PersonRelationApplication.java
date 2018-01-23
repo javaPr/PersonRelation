077668f9a2853d9caeab25f7e6094a0f3935e26c
dechangwang <13changice@tongji.edu.cn>
2018-01-23 21:32:58 +0800

    init

---
+++ src/main/java/com/ieven/PersonRelationApplication.java
@@ -0,0 +1,12 @@
+package com.ieven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonRelationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonRelationApplication.class, args);
+	}
+}
